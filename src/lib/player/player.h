2af8f50b928143ba969d59b27012559911224334
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-06-20 16:02:38 -0300

    Add the possibility to modify Player symbol.

--- src/lib/player/player.h
+++ src/lib/player/player.h
@@ -1,50 +1,62 @@
-#ifndef TICTACTOE_PLAYER_H
-#define TICTACTOE_PLAYER_H
+    #ifndef TICTACTOE_PLAYER_H
+    #define TICTACTOE_PLAYER_H
 
-#include <limits>
-#include <list>
-#include <string>
+    #include <limits>
+    #include <list>
+    #include <string>
 
-#include "../table/plain_table.h"
-#include "player_move.h"
-#include "../exceptions/player/player_exceptions.h"
+    #include "../table/plain_table.h"
+    #include "player_move.h"
+    #include "../exceptions/player/player_exceptions.h"
 
-namespace tic_tac_toe {
+    namespace tic_tac_toe {
     using rowsAndColumns = std::vector<std::vector<unsigned short>>;
 
     /*
-     * Players of the tic tac toe game.
-     *
-     * Symbols for each player should be different from others players
-     * otherwise an UnusableSymbol exception will be thrown.
-     *
-     * The symbol is freed after the Player deleter is called.
-     */
+    * Players of the tic tac toe game.
+    *
+    * Symbols for each player should be different from others players
+    * otherwise an UnusableSymbol exception will be thrown.
+    *
+    * The symbol is freed after the Player deleter is called.
+    */
     template<typename T>
     class Player {
 
     /**************************************************************
-     *                     Variable Definition                    *
-     **************************************************************/
+        *                     Variable Definition                    *
+        **************************************************************/
     private:
         static std::list<T> players_symbols;
         T player_symbol;
 
     /**************************************************************
-     *                     Function Definition                    *
-     **************************************************************/
+        *                     Function Definition                    *
+        **************************************************************/
     public:
-       // Return the player symbol.
-       const T & getPlayerSymbol() const {
-           return this->player_symbol;
-       }
+        // Return the player symbol.
+        const T & getPlayerSymbol() const {
+            return this->player_symbol;
+        }
 
-       // Make the player act by getting its move intent.
-       virtual PlayerMove<T> getMove(PlainTable<T> &table) = 0;
+        /*
+        * Set the player symbol while veryfing if such
+        * is used. If used, UnsuableSymbol exception will be thrown.
+        */
+        void setPlayerSymbol(T & symbol) {
+            if (!verifyIfUsedSymbol(symbol)) {
+                this->player_symbol = symbol;
+            } else {
+                throw UnusableSymbol();
+            }
+        }
 
-       virtual ~Player() {
-           Player<T>::players_symbols.remove(this->player_symbol);
-       }
+        // Make the player act by getting its move intent.
+        virtual PlayerMove<T> getMove(PlainTable<T> &table) = 0;
+
+        virtual ~Player() {
+            Player<T>::players_symbols.remove(this->player_symbol);
+        }
     protected:
         // Verify in our list of player symbols if there's match. If so,
         // returns true otherwise false.
@@ -86,7 +98,7 @@ namespace tic_tac_toe {
 
     template<class T>
     std::list<T> Player<T>::players_symbols;
-}
+    }
 
 
-#endif //TICTACTOE_PLAYER_H
+    #endif //TICTACTOE_PLAYER_H
