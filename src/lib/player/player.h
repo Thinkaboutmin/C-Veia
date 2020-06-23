6c6a34c29d5d3860943c6362bbb4a18f42f68322
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-06-23 16:25:51 -0300

    Reduce memory footprint in Player

--- src/lib/player/player.h
+++ src/lib/player/player.h
@@ -10,94 +10,95 @@
 #include "../exceptions/player/player_exceptions.h"
 
 namespace tic_tac_toe {
-using rowsAndColumns = std::vector<std::vector<unsigned short>>;
-
-/*
-* Players of the tic tac toe game.
-*
-* Symbols for each player should be different from others players
-* otherwise an UnusableSymbol exception will be thrown.
-*
-* The symbol is freed after the Player deleter is called.
-*/
-template<typename T>
-class Player {
-
-/**************************************************************
-*                     Variable Definition                    *
-**************************************************************/
-private:
-    static std::list<T> players_symbols;
-    T player_symbol;
-
-/**************************************************************
-*                     Function Definition                    *
-**************************************************************/
-public:
-    // Return the player symbol.
-    const T & getPlayerSymbol() const {
-        return this->player_symbol;
-    }
+    using rowsAndColumns = std::vector<std::vector<unsigned short>>;
 
     /*
-    * Set the player symbol while veryfing if such
-    * is used. If used, UnsuableSymbol exception will be thrown.
+    * Players of the tic tac toe game.
+    *
+    * Symbols for each player should be different from others players
+    * otherwise an UnusableSymbol exception will be thrown.
+    *
+    * The symbol is freed after the Player deleter is called.
     */
-    void setPlayerSymbol(T & symbol) {
-        if (!verifyIfUsedSymbol(symbol)) {
-            this->player_symbol = symbol;
-        } else {
-            throw UnusableSymbol();
+    template<typename T>
+    class Player {
+
+    /**************************************************************
+    *                     Variable Definition                    *
+    **************************************************************/
+    private:
+        static std::list<const T *> players_symbols;
+        T player_symbol;
+
+    /**************************************************************
+    *                     Function Definition                    *
+    **************************************************************/
+    public:
+        // Return the player symbol.
+        const T & getPlayerSymbol() const {
+            return this->player_symbol;
         }
-    }
 
-    // Make the player act by getting its move intent.
-    virtual PlayerMove<T> getMove(PlainTable<T> &table) = 0;
-
-    virtual ~Player() {
-        Player<T>::players_symbols.remove(this->player_symbol);
-    }
-protected:
-    // Verify in our list of player symbols if there's match. If so,
-    // returns true otherwise false.
-    static bool verifyIfUsedSymbol(const T& symbol) {
-        for (const T& list_symbol : Player<T>::players_symbols) {
-            if (list_symbol == symbol) {
-                return true;
+        /*
+        * Set the player symbol while veryfing if such
+        * is used. If used, UnsuableSymbol exception will be thrown.
+        */
+        void setPlayerSymbol(T & symbol) {
+            if (!verifyIfUsedSymbol(symbol)) {
+                this->player_symbol = symbol;
+            } else {
+                throw UnusableSymbol();
             }
         }
 
-        return false;
-    }
+        // Make the player act by getting its move intent.
+        virtual PlayerMove<T> getMove(PlainTable<T> &table) = 0;
 
-    // Return a std::vector<std::vector<unsigned short>> with
-    // all the rows and columns which still have a default type.
-    static rowsAndColumns getAvailableMoves(PlainTable<T> &table) {
-        rowsAndColumns availableCells;
-
-        for (unsigned short row = 1; row <= table.getRowsNum(); ++row) {
-            availableCells.emplace_back(std::vector<unsigned short>());
-            for (unsigned short column = 1; column <= table.getColumnsNum(); ++column) {
-                if (*table.getCellValue(row, column) == table.getEmptyValue()) {
-                    availableCells[row - 1].emplace_back(column);
+        virtual ~Player() {
+            Player<T>::players_symbols.remove(&this->player_symbol);
+        }
+    protected:
+        // Verify in our list of player symbols if there's match. If so,
+        // returns true otherwise false.
+        static bool verifyIfUsedSymbol(const T& symbol) {
+            for (const T * list_symbol : Player<T>::players_symbols) {
+                if ((*list_symbol) == symbol) {
+                    return true;
                 }
             }
+
+            return false;
         }
 
-        return availableCells;
-    }
+        // Return a std::vector<std::vector<unsigned short>> with
+        // all the rows and columns which still have a default type.
+        static rowsAndColumns getAvailableMoves(PlainTable<T> &table) {
+            rowsAndColumns availableCells;
 
-    Player(const T& symbol) {
-        if (!verifyIfUsedSymbol(symbol)) {
-            this->player_symbol = symbol;
-        } else {
-            throw UnusableSymbol();
+            for (unsigned short row = 1; row <= table.getRowsNum(); ++row) {
+                availableCells.emplace_back(std::vector<unsigned short>());
+                for (unsigned short column = 1; column <= table.getColumnsNum(); ++column) {
+                    if (*table.getCellValue(row, column) == table.getEmptyValue()) {
+                        availableCells[row - 1].emplace_back(column);
+                    }
+                }
+            }
+
+            return availableCells;
         }
-    }
-};
 
-template<class T>
-std::list<T> Player<T>::players_symbols;
+        Player(const T& symbol) {
+            if (!verifyIfUsedSymbol(symbol)) {
+                this->player_symbol = symbol;
+                Player<T>::players_symbols.emplace_back(&this->player_symbol);
+            } else {
+                throw UnusableSymbol();
+            }
+        }
+    };
+
+    template<class T>
+    std::list<const T *> Player<T>::players_symbols;
 }
 
 
