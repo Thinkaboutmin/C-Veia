47ab77cdc7448639c3f98976743e55cb8ef7d59f
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-06-24 15:52:33 -0300

    Change the string table generation a bit. This changes would be useful for an adaptive size of the column but it would affect all others. It will be kept because it works and it has been done.

--- src/cli/string_table.cpp
+++ src/cli/string_table.cpp
@@ -12,24 +12,26 @@ namespace tic_tac_toe {
         // Kamehameha!!! Sry...
         if (this->getColumnsNum() != 0 && this->getRowsNum() != 0) {
             for (unsigned short row_x = 1; row_x <= this->getRowsNum(); ++row_x) {
-                std::wstring downDivisor;
+                std::wstring down_divisor;
                 for (unsigned short column_y = 1; column_y <= this->getColumnsNum(); ++column_y) {
-                    table += L" " + *getCellValue(row_x, column_y) + L" ";
+                    table += this->getEmptyValue() + *getCellValue(row_x, column_y) + this->getEmptyValue();
+                    std::wstring::size_type size = getCellValue(row_x, column_y)->size();
 
                     // Verify if it is the last column meaning that there should not
                     // be a divisor for the table.
                     // Same but with the row. We will add the bottom divisor.
                     if (column_y != this->getRowsNum()) {
+                        down_divisor.insert(down_divisor.begin() + down_divisor.size() , size + 2, L'\u2550');
+                        down_divisor += L"\u256c";
                         table += L"\u2551";
-                        downDivisor += L"\u2550\u2550\u2550\u256c";
                     } else {
-                        downDivisor += L"\u2550\u2550\u2550";
+                        down_divisor.insert(down_divisor.begin() + down_divisor.size(), size + 2, L'\u2550');
                     }
                 }
                 // Check if this is the last run. If so, do not add
                 // bottom divisor.
                 if (row_x + 1 <= this->getRowsNum()) {
-                    table += L"\n" + downDivisor + L"\n";
+                    table += L"\n" + down_divisor + L"\n";
                 }
             }
         }
