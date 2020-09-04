70e908039e1f56d31a73b4a8f14593f6314e8a2f
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-09-04 18:37:46 -0300

    add namespace to difficulty enum

--- src/lib/player/players_enum.h
+++ src/lib/player/players_enum.h
@@ -1,11 +1,13 @@
 #ifndef TICTACTOE_PLAYERS_ENUM_H
 #define TICTACTOE_PLAYERS_ENUM_H
 
-enum difficulty {
-    DEFAULT, // Normal
-    EASY,
-    NORMAL,
-    HARD
-};
+namespace tic_tac_toe {
+    enum difficulty {
+        DEFAULT, // Normal
+        EASY,
+        NORMAL,
+        HARD,
+    };
+}
 
 #endif //TICTACTOE_PLAYERS_ENUM_H
